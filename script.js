165b9a50fb374ea54479af7d7b0c30df40bf055d
KirillKB46 <99921817+KirillKB46@users.noreply.github.com>
2022-02-25 01:09:29 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-console.log([1, 2, 3, 4, 5, 6, 7, 8].filter(Element => !(Element % 2)).reduce((accumulator, Element) => accumulator + Math.sqrt(Element), 0));
-
 console.log(1.15 + 2.3);
 
 let a = "";
